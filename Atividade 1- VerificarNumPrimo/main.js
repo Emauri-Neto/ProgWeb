0e67a39e78716d0906c7e10fff1ef266c13da525
Emauri-Neto <emaurigomesneto@gmail.com>
2023-08-12 15:36:46 -0300

    Adicionando a atividade 1

---
+++ Atividade 1- VerificarNumPrimo/main.js
@@ -0,0 +1,12 @@
+let num = 5;
+
+for (let p = 2; p < num; p++) {
+    if (num % p == 0) {
+        console.log(0);
+        break;
+    }
+    else {
+        console.log(1);
+        break;
+    }
+}
